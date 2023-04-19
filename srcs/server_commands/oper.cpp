47492b666d541634cfcff69f095dd92e56f24bd1
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-04-19 09:55:53 +0200

    add err_needmoreparams to oper

--- srcs/server_commands/oper.cpp
+++ srcs/server_commands/oper.cpp
@@ -29,19 +29,22 @@ check_oper_pwd(std::string pwd, std::vector<server_oper> oper_list)
 void
 Client::oper(void)
 {
-    std::cout << "***SERVER OPERS***" << std::endl;
-    for (std::vector<server_oper>::iterator it = _server->_server_opers.begin(); it != _server->_server_opers.end(); it++)
-    {
-        std::cout << it->name << std::endl;
-        std::cout << it->pwd << std::endl;
-        std::cout << it->host << std::endl;
-        std::cout << std::endl;
-    }
+    // std::cout << "***SERVER OPERS***" << std::endl;
+    // for (std::vector<server_oper>::iterator it = _server->_server_opers.begin(); it != _server->_server_opers.end(); it++)
+    // {
+    //     std::cout << it->name << std::endl;
+    //     std::cout << it->pwd << std::endl;
+    //     std::cout << it->host << std::endl;
+    //     std::cout << std::endl;
+    // }
 
     if (_connected == false)
         return;
-    if (_parsed_cmd.size() != 3) // on a besoin de name et password
+    if (_parsed_cmd.size() < 3) // on a besoin de name et password
+    {
+        send(_client_id, ERR_NEEDMOREPARAMS(_nick, "OPER").c_str(), ERR_NEEDMOREPARAMS(_nick, "OPER").size(), 0);
         return;
+    }
     if (check_oper_name(_parsed_cmd[1], _server->_server_opers) == false)
     {
     	send(_client_id, ERR_NOOPERHOST(_nick).c_str(), ERR_NOOPERHOST(_nick).size(), 0);
